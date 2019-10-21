b2c5c811155d5d912389d94aa55ebdf2d2d42755
nazilnazi <ndlhassan@gmail.com>
2019-10-21 10:40:46 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text,Button, View} from 'react-native';
+
+
+class Home extends React.Component{
+    render() {
+        return (
+            <View style={{ flex:1, alignItems: "flex-end", justifyContent: 'center' }}>
+                <Text>Hellowooooo</Text>
+            </View>
+        );
+    }
+}
+
+export default Home;
